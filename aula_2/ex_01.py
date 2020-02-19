c3b061348ccce6695b3ba997caf45f914dbf40da
Ivan Ferro <iaferro@gmail.com>
2020-02-18 22:16:54 -0300

    aula 2

---
+++ aula_2/ex_01.py
@@ -0,0 +1,11 @@
+idade = input ('digite sua idade: ')
+
+numerais = ('0123456789')
+
+for letra in numerais:
+    if letra in numerais:
+        print ('digitou um numero')
+    else: 
+        print ('digitou um caracter nao numerico')
+
+# idade = int(idade)
---
+++ aula_2/for.py
@@ -0,0 +1,18 @@
+# imprime os numeros de 10 ate 98
+for i in range(10.99):
+    print(i)
+
+    lista = [1,2,3,4,5,6,7,8,9]
+    soma = 0
+    
+
+# imprime os numeros de 1  ate 9
+
+for numero in lista:
+    soma = soma + numero
+    contador = contador + 1
+    print(soma)
+
+media = soma / contador
+
+print(media)
