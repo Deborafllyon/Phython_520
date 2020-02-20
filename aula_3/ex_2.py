d11265bbee1f2320743f87f1a1e07f072b93dc4c
Ivan Ferro <iaferro@gmail.com>
2020-02-19 22:24:18 -0300

    aula3

---
+++ aula_3/ex_2.py
@@ -0,0 +1,18 @@
+
+def calcular_media(Listas_de_notas):
+    resultado = 0
+    contador = 0
+    for nota in lista_de_notas:
+        resultado = resultado + nota
+        contador = contador + 1
+    return resultado / contador
+    print(media)
+
+
+
+
+
+lista_de_notas = [6,4,1,8,10,1]
+
+media = calcular_media(lista_de_notas)
+print(media)
